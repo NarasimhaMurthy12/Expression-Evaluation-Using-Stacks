cc05cf425489fae076dba2f27e615e6df08cf847
NarasimhaMurthy12 <narasimhamurthypanchangam@gmail.com>
2025-04-05 14:38:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 void main()
 {
-	char inpt[100];
-	char postfi[100];
+	char inpt[50];
+	char postfi[50];
 	printf("Enter the expression with spaces between th operand and operator : ");
-	fgets(inpt,20,stdin);
+	fgets(inpt,50,stdin);
 	char *postfix=infix_to_postfix(inpt,postfi);
 	postfix_eval(postfix);
 }
